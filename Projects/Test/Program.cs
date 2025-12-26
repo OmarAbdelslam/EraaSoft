2fb0c3adf6a754fd2baf2a2f7008df0ace593f99
OmarAbdelslam <https://www.github.com/OmarAbdelslam>
2025-12-27 00:19:50 +0200

    Add projects folder with all assignment

---
+++ Projects/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test
+{
+
+    public class Program
+    {
+        private int[] array = { 1, 2, 3, 4, 5 };
+        public int this[int a] => array[a];
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine( program[0]);
+            Console.ReadKey();
+        }
+    }
+}
