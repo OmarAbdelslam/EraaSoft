7a0adc32b42dcdae48dd86b24d1034d72ae383cb
OmarAbdelslam <https://www.github.com/OmarAbdelslam>
2026-01-31 01:48:29 +0200

    Update Lecuture 7

--- Projects/Lecture7Database/SQLQuery1.sql
+++ Projects/Lecture7Database/SQLQuery1.sql
@@ -36,4 +36,59 @@ ProjectName varchar(50)
 --6
 
 Alter Table Employees
-Add Constraint ID primary key
+Alter Column ID int not null ;
+
+Alter Table Employees
+Add Constraint ID_Primarykey primary key (ID);
+
+--7
+
+Alter Table Employees
+Add Constraint AddUniqueConstraint Unique ([Name])
+
+--8
+
+Create Table Customers
+(
+CustomerID int ,
+FirstName varchar(50) ,
+LastName  varchar(50) ,
+Email varchar(100) ,
+[Status] varchar(50)
+)
+
+--9
+
+Alter Table Customers
+Add Constraint DoubleUniqueConstraint unique (FirstName,LastName)
+
+--10
+
+Create Table Orders
+(
+OrderID int ,
+CustomerID int ,
+OrderDate DateTime ,
+TotalAmount decimal
+)
+
+--11
+
+
+Alter Table Orders
+Add Constraint CheckConstraintTotalAmount Check(TotalAmount > 0)
+
+--12
+
+Create Schema Sales ;
+
+
+ALTER SCHEMA Sales 
+    TRANSFER dbo.Orders
+
+--13
+
+EXEC sp_rename 'Sales.Orders', 'SalesOrders'
+
+
+
