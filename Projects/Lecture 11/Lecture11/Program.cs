47f6dee47205ad14d86be083a413209c419cc56e
OmarAbdelslam <https://www.github.com/OmarAbdelslam>
2026-03-13 23:40:50 +0200

    Lecture 11

---
+++ Projects/Lecture 11/Lecture11/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Lecture11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+           
+              "Data Source=.;Initial Catalog=BikeStores;Integrated Security=SSPI;"
+            
+
+        }
+    }
+}
